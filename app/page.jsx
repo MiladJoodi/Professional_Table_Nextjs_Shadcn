383507e434ccafef08795b3c137a87041ff8ebf5
Milad Joodi <winbeta.com@gmail.com>
2024-03-12 15:36:03 +0330

    update

--- app/page.jsx
+++ app/page.jsx
@@ -6,8 +6,8 @@ export default function Home({searchParams}) {
 
   const query = searchParams?.query || ''
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SearchBar placeHolder="Search nvoice..." />
+    <main className="flex min-h-screen flex-col justify-between p-24">
+      <SearchBar placeHolder="Search invoice..." />
       <TableNew query={query} />
     </main>
   );
--- components/SearchBar/SearchBar.jsx
+++ components/SearchBar/SearchBar.jsx
@@ -5,36 +5,39 @@ import { Input } from "@/components/ui/input"
 import { useSearchParams, usePathname, useRouter } from "next/navigation"
 import { useDebouncedCallback } from "use-debounce"
 
-const SearchBar = ({placeHolder}) => {
+const SearchBar = ({ placeHolder }) => {
 
   const searchParams = useSearchParams();
 
   const pathname = usePathname();
-  const {replace} = useRouter()
+  const { replace } = useRouter()
 
 
-  const handleSearch = useDebouncedCallback((term)=> {
+  const handleSearch = useDebouncedCallback((term) => {
     const params = new URLSearchParams(searchParams);
-    
-    if(term){
+
+    if (term) {
       params.set('query', term)
-    }else{
+    } else {
       params.delete('query')
     }
     replace(`${pathname}?${params.toString()}`)
-  },2000)
+  }, 2000)
 
-    return (
-        <div className="flex gap-2 w-full max-w-sm items-center space-x-2">
-      <Input 
-      type="email" 
-      placeholder={placeHolder}
-      onChange={(e)=>{handleSearch(e.target.value)}}
-      defaultValue={searchParams.get('query')?.toString()}
-      />
-      <Button type="submit">Subscribe</Button>
+  return (
+    <div className="flex gap-2 w-full max-w-sm items-center space-x-2" style={{"gap" : "15px"}}>
+        <Input
+          type="email"
+          placeholder={placeHolder}
+          onChange={(e) => { handleSearch(e.target.value) }}
+          defaultValue={searchParams.get('query')?.toString()}
+          className="w-4/5"
+        />
+
+        <Button type="submit" className="ml-2">Search</Button>
+
     </div>
-    );
+  );
 }
 
 export default SearchBar;
